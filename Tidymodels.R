0f55335992a7cd34f1047c85371021e4a23d0e1b
BradCamelo <bcamelo@tce.pb.gov.br>
2024-06-09 12:04:34 -0300

    Novo Commit

--- Tidymodels.R
+++ Tidymodels.R
@@ -41,30 +41,6 @@ dados_treinamento <- training(dados_split)
 # Dados de teste
 dados_teste <- testing(dados_split)
 
-<<<<<<< HEAD
-# Receita -----------------------------------------------------------------
-=======
-df_top_padrao <- df_top_padrao %>%
-  select( c(Custo_Tran_norm, Quantidade, idhm, gini, area_km2, 
-            populacao, Tempo_Medio, distancia, ReceitaCorrente, 
-            Licit, n_licit, Precatorio) )
-
-#data_subset <- data_subset %>% slice_sample(prop = 0.7)
-
-df_top_padrao <- df_top_padrao %>%
-  mutate(across(where(is.numeric),
-                ~ ifelse(is.infinite(.), NA, .))) %>%
-  mutate(distancia = log(distancia + 1)) 
-
-
-set.seed(58046088)
-df_split <- initial_split(df_top_padrao, strata = Custo_Tran_norm, prop = 3/4)
-df_train <- training(df_split)
-df_test  <- testing(df_split)
-
-
-df_folds <- 
-  vfold_cv(df_train, strata = Custo_Tran_norm, repeats = 5)
 
 ## Dimensionalidade
 
@@ -107,8 +83,6 @@ ggplot(df_top_padrao, aes(Custo_Tran_norm)) +
   theme_bw()
 
 
-## Baseado no script do Gabriel
-
 # Modelo de regressão linear
 
 modelo <- lm(Custo_Tran_norm ~ Quantidade + idhm + populacao + Tempo_Medio + 
@@ -121,19 +95,9 @@ summary(modelo)
 
 
 
-# Filtrar as colunas necessárias e criar o gráfico de pares
-
-df_train %>%
-  select(Custo_Tran_norm, idhm, populacao,
-         Tempo_Medio, distancia, ReceitaCorrente, Licit,
-         n_licit, Precatorio) %>%
-  ggpairs(axisLabels = "none") +
-  theme_bw()
-
-
 #Distribuição das variáveis independentes
 
-df_train %>%
+dados_treinamento %>%
   select( c(Quantidade, `Município Destinatário`, idhm,
             gini, area_km2, populacao, Tempo_Medio, distancia, 
             ReceitaCorrente, Licit, n_licit, Precatorio) ) %>% 
@@ -148,252 +112,12 @@ df_train %>%
 
 ## receita
 
-# Selecionar apenas colunas numéricas
-df_numeric <- df_train %>% select_if(is.numeric)
-
-# Calcular a assimetria (skewness) e variância para cada variável numérica
-skewness_values <- sapply(df_numeric, skewness, na.rm = TRUE)
-variance_values <- sapply(df_numeric, var, na.rm = TRUE)
-
-# checagem
-print("Assimetria das variáveis:")
-print(skewness_values)
-print("Variância das variáveis:")
-print(variance_values)
-
-# Identificar variáveis numéricas com assimetria significativa para transformação logarítmica
-numeric_skewed <- names(skewness_values[skewness_values > 1 | skewness_values < -1])
-
-# Adicionar transformações ao recipe
-recipe_rf <- recipe(Custo_Tran_norm ~ ., data = df_train) |>
-  step_log(all_of(numeric_skewed), base = exp(1)) |>
-  step_dummy(all_nominal_predictors()) |> 
-  step_impute_bag(all_numeric_predictors()) |>
-  step_zv(all_predictors()) |>
-  step_nzv(all_predictors()) |>
-  step_center(all_numeric_predictors()) |>
-  step_scale(all_numeric_predictors())
-
-
-# receita individual para lasso
-recipe_lasso <-  
-  df_train |>
-  recipe(Custo_Tran_norm ~ .) |>
-  # criar variáveis dummies com cada classe de variáveis categóricas
-  step_dummy(all_nominal_predictors()) |> 
-  step_impute_bag(all_numeric_predictors()) |>
-  step_normalize(all_numeric_predictors()) |> 
-  step_zv(all_predictors()) |>
-  step_nzv(all_predictors()) |> 
-  step_factor2string(all_nominal_predictors())
-
-# workflow
-# o workflow serve para juntar a parte de preprocessamento e o modelo a ser utilizado
-
-# workflow para random forest
-workflow_rf <- 
-  workflow() |>
-  # modelo random forest
-  add_model(rand_forest(mtry = tune(), trees = tune(), min_n = 2) |>
-              set_engine("ranger", importance = "impurity", seed = 1) |>
-              set_mode("regression")) |>
-  # adicionar receita 
-  add_recipe(recipe_rf)
-
-
-# workflow para lasso
-workflow_lasso <- workflow() |>
-  # modelo lasso
-  add_model(linear_reg(penalty = tune(), mixture = 1) |>
-              set_engine("glmnet")
-  ) |>
-  # adiciona receita do lasso
-  add_recipe(recipe_lasso)
-
-
-# a otimização do modelo pode ser feita utilizando grid search, encontrar a melhor combinação entre os hiperparametros do modelo
-
-# grid para random forest
-tree_grid <- grid_regular(trees(range = c(350, 1000)),
-                          mtry(range = c(7, 13)),
-                          levels = 5)
-
-# grid para lasso
-lasso_grid <- grid_regular(penalty(),
-                           levels = 50)
-
-# criação de fold para observar as métricas do modelo em cada fold e tomar o melhor entre eles
-folds <- vfold_cv(df_train, v = 5)
-
-# tunagem random forest
-tree_res <-
-  workflow_rf |>
-  tune_grid(
-    resamples = folds,
-    metrics = metric_set(rsq, rmse),
-    grid = tree_grid
-  )
-
-# tunagem lasso
-lasso_res <-
-  workflow_lasso |>
-  tune_grid(
-    resamples = folds,
-    metrics = metric_set(rsq, rmse),
-    grid = lasso_grid
-  )
-
-#Métricas para cada combinação no Grid Search RF
-
-tree_res %>%
-  collect_metrics() %>%
-  mutate(trees = factor(trees)) %>%
-  ggplot(aes(mtry, mean, color = trees)) +
-  geom_line(size = 1.5, alpha = 0.7) +
-  geom_point(size = 2) +
-  facet_wrap(~ .metric, scales = "free", nrow = 2) +
-  scale_x_log10(labels = scales::label_number()) +
-  scale_color_viridis_d(option = "plasma", begin = .9, end = 0) +
-  ylab("Média") +
-  xlab("Quantidade de variáveis independentes") +
-  theme_bw()
-
-# Métricas para cada combinação no Grid Search em cada Fold (Lasso)
-
-lasso_res |> 
-  collect_metrics() |> 
-  ggplot(aes(penalty, mean)) +
-  geom_line(size = 1.5, alpha = 0.6) +
-  geom_point(size = 2) +
-  facet_wrap(~ .metric, scales = "free", nrow = 2) +
-  scale_x_log10(labels = scales::label_number()) +
-  scale_color_viridis_d(option = "plasma", begin = .9, end = 0) +
-  ylab("Média") +
-  xlab("Penalty") +
-  theme_bw()
-
-#Valores previstos x observados
-
-# modelo lasso com o melhor penalty encontrados pelo grid search
-final_wf_lasso <- 
-  workflow_lasso %>% 
-  finalize_workflow(
-    lasso_res |> 
-      select_best("rmse")
-  ) |> 
-  last_fit(df_split)
-
-
-# modelo random forest com os melhores hiperparametros encontrados pelo grid search
-final_wf_rf <- 
-  workflow_rf %>% 
-  finalize_workflow(
-    tree_res |> 
-      select_best(metric = "rmse")
-  ) |> 
-  last_fit(df_split)
-
-
-final_wf_rf |> 
-  collect_predictions() |>
-  ggplot(aes(Custo_Tran_norm, .pred))+
-  geom_point() +
-  geom_abline(slope = 1, intercept = 0, color = "red",
-              size = 1.5, linetype = "dashed") +
-  theme_bw() +
-  ylab("Valores Previstos") +
-  xlab("Valores Observados")
-
-
-final_wf_lasso |> 
-  collect_predictions() |>
-  ggplot(aes(Custo_Tran_norm, .pred))+
-  geom_point() +
-  geom_abline(slope = 1, intercept = 0, color = "red",
-              size = 1.5, linetype = "dashed") +
-  theme_bw() +
-  ylab("Valores Previstos") +
-  xlab("Valores Observados")
-
-#Variáveis mais importantes na Random Forest
-
-final_wf_rf |> 
-  extract_fit_engine() |> 
-  vip() +
-  theme_bw()
-
-# Previsões RF
-
-rf_fit <- fit(workflow_rf, train_data)
-
-
-predictions <- predict(rf_fit, test_data, type = "regression") %>%
-  bind_cols(test_data)
-
-# Calcular métricas
-results <- predictions %>%
-  metrics(truth = Custo_Tran_norm, estimate = .pred) %>%
-  add_metric(metric = rmse, truth = Custo_Tran_norm, estimate = .pred) %>%
-  add_metric(metric = rsq, truth = Custo_Tran_norm, estimate = .pred)
-
-
-
-### RF
-
-recipe_rf <- recipe(Custo_Tran_norm ~ Quantidade + `Município Destinatário` + 
-                      Custo_Tran + pct + idhm + idhm_edu + idhm_long +
-                      idhm_renda + gini + area_km2 + populacao + Tempo_Medio + 
-                      distancia + ReceitaCorrente + Licit +
-                      n_licit + Precatorio, data = df_train) %>%
-  step_normalize(all_numeric(), -all_outcomes()) %>%
-  step_dummy(all_nominal(), -all_outcomes())
-
-# Especificar o modelo Random Forest
-model_rf <- rand_forest(trees = 1000, mode = "regression") %>%
-  set_engine("ranger") %>%
-  set_mode("regression")
-
-# Criar um fluxo de trabalho combinando a receita e o modelo
-workflow_rf <- workflow() %>%
-  add_recipe(recipe_rf) %>%
-  add_model(model_rf)
-
-# Treinar o modelo
-fit_rf <- workflow_rf %>%
-  fit(data = df_train)
-
-# Avaliar o modelo no conjunto de teste
-
-results <- fit_rf %>%
-  predict(test_data) %>%
-  bind_cols(test_data) %>%
-  metrics(truth = Custo_Tran_norm, estimate = .pred)
-
-# Imprimir os resultados das métricas
-print(results)
-
-# Ou, se quiser ver a importância das variáveis:
-
-vip <- fit_rf %>%
-  pull_workflow_fit() %>%
-  vip(geom = "col")
-
-print(vip)
-
-
-
-
-
-## Receita
-
-
->>>>>>> parent of e18ae2d (Novo commit)
 receita <- 
   dados_treinamento |>
   recipes::recipe(custo_tran_norm ~ ., data = _) |>
   recipes::step_dummy(all_nominal_predictors()) |> 
   recipes::step_nzv(all_predictors()) |>
-  recipes::step_impute_knn(all_numeric_predictors(), impute_with = imp_vars(quantidade, precatorio)) |>
+  recipes::step_impute_knn(all_numeric_predictors(), impute_with = imp_vars(quantidade, valor_unitario, valor_produtos, precatorio, preco)) |>
   step_corr(all_numeric(), -all_outcomes(), threshold = 0.9)
 
 # Pre-processamento separado do treinamento -------------------------------
@@ -419,7 +143,7 @@ tunagem <-
     wf,
     resamples = cv,
     grid = 20L,
-    metrics = metric_set(rsq, yardstick::rmse),
+    metrics = metric_set(rsq, rmse),
     control = control_grid(save_pred = TRUE, verbose = TRUE, allow_par = FALSE)
   )
 
@@ -441,7 +165,7 @@ modelo <-
   last_fit(
     wf, 
     dados_split, 
-    metrics = metric_set(rsq, yardstick::rmse)
+    metrics = metric_set(rsq, rmse)
   )
 
 # Visualizando as métricas finais -----------------------------------------
@@ -457,16 +181,16 @@ saveRDS(modelo_final, file = "modelo_random_forest.rds")
 modelo_carregado <- readRDS("modelo_random_forest.rds")
 
 # Fazendo previsões no conjunto de dados de teste
-previsoes <- predict(modelo_carregado, new_data = dados_treinamento)
+previsoes <- predict(modelo_carregado, new_data = dados)
 
 # Combinando previsões com os valores reais
-resultados <- bind_cols(dados_treinamento_preparados, previsoes) |>
+resultados <- bind_cols(dados_teste_preparados, previsoes) |>
   rename(previsao = .pred)
 
 # Combinando previsões com os valores reais
-#resultados <- bind_cols(real = dados$custo_tran_norm, estimado = previsoes) 
+resultados <- bind_cols(real = dados$custo_tran_norm, estimado = previsoes) 
 
-ggplot(resultados, aes(x = custo_tran_norm, y = previsao)) +
+ggplot(resultados, aes(x = real, y = .pred)) +
   geom_point(alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0, color = "red", linetype = "dashed") +
   labs(
@@ -495,3 +219,98 @@ metrics <- resultados |>
 print(metrics)
 
 kable(metrics, format = "latex", booktabs = TRUE)
+
+## LASSO-----
+
+recipe_lasso <-  
+  dados_treinamento |>
+  recipe(custo_tran_norm ~ .) |>
+  step_dummy(all_nominal_predictors()) |> 
+  step_impute_median(all_numeric_predictors()) |>  
+  step_impute_mode(all_nominal_predictors()) |>    
+  step_normalize(all_numeric_predictors()) |> 
+  step_zv(all_predictors()) |>
+  step_nzv(all_predictors()) |> 
+  step_factor2string(all_nominal_predictors())
+
+workflow_lasso <- workflow() |>
+  add_model(linear_reg(penalty = tune(), mixture = 1) |>
+              set_engine("glmnet")
+  ) |>
+  add_recipe(recipe_lasso)
+
+# grid para lasso
+lasso_grid <- grid_regular(penalty(),
+                           levels = 50)
+
+
+folds <- vfold_cv(dados_treinamento, v = 5)
+
+lasso_res <-
+  workflow_lasso |>
+  tune_grid(
+    resamples = folds,
+    metrics = metric_set(rsq, rmse),
+    grid = lasso_grid
+  )
+
+
+lasso_res |> 
+  collect_metrics() |> 
+  ggplot(aes(penalty, mean)) +
+  geom_line(size = 1.5, alpha = 0.6) +
+  geom_point(size = 2) +
+  facet_wrap(~ .metric, scales = "free", nrow = 2) +
+  scale_x_log10(labels = scales::label_number()) +
+  scale_color_viridis_d(option = "plasma", begin = .9, end = 0) +
+  ylab("Média") +
+  xlab("Penalty") +
+  theme_bw()
+
+
+# Selecionar o melhor modelo com base no RMSE
+best_lasso <- lasso_res %>% 
+  select_best(metric = "rmse")
+
+# Finalizar o workflow com os melhores hiperparâmetros
+final_wf_lasso <- 
+  workflow_lasso %>% 
+  finalize_workflow(best_lasso)
+
+# Ajustar o modelo final com o conjunto de dados de treino e teste
+final_fit <- final_wf_lasso %>% 
+  last_fit(dados_split)
+
+#gráficos
+final_fit |> 
+  collect_predictions() |>
+  ggplot(aes(custo_tran_norm, .pred))+
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, color = "red",
+              size = 1.5, linetype = "dashed") +
+  theme_bw() +
+  ylab("Valores Previstos") +
+  xlab("Valores Observados")
+
+
+# Métricas para cada combinação no Grid Search em cada Fold (Lasso)
+
+lasso_res |> 
+  collect_metrics() |> 
+  ggplot(aes(penalty, mean)) +
+  geom_line(size = 1.5, alpha = 0.6) +
+  geom_point(size = 2) +
+  facet_wrap(~ .metric, scales = "free", nrow = 2) +
+  scale_x_log10(labels = scales::label_number()) +
+  scale_color_viridis_d(option = "plasma", begin = .9, end = 0) +
+  ylab("Média") +
+  xlab("Penalty")
+
+# Calculando as métricas R² e RMSE
+
+final_metrics <- final_fit %>%
+  collect_metrics()
+
+# Visualizando as métricas
+
+kable(final_metrics, format = "latex", booktabs = TRUE)
